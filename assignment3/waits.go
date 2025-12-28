c95485fd5242390dfbf2cb1fadb263988a6aba13
axmdl1 <mr.akhmedali@bk.ru>
2025-12-29 01:25:22 +0500

    explicit

--- assignment3/waits.go
+++ assignment3/waits.go
@@ -28,21 +28,37 @@ func main() {
 
 	driver.Get("https://github.com")
 
-	emailInput, err := driver.FindElement(selenium.ByName, "user_email")
+	searchBtn, err := driver.FindElement(selenium.ByCSSSelector, "button.header-search-button")
 	if err != nil {
-		panic("Search input not found")
+		log.Fatalf("Error while clicking search button: %v", err)
 	}
+	searchBtn.Click()
 
-	emailInput.SendKeys("selenium webdriver, Akhmedali!")
+	driver.SetImplicitWaitTimeout(5 * time.Second)
 
-	driver.SetImplicitWaitTimeout(10 * time.Second)
+	var realInput selenium.WebElement
+	err = driver.WaitWithTimeout(func(wd selenium.WebDriver) (bool, error) {
+		el, err := wd.FindElement(selenium.ByID, "query-builder-test")
+		if err != nil {
+			return false, nil
+		}
+		realInput = el
+		return true, nil
+	}, 10*time.Second)
 
-	//Not working well continue here.
-	searchInput, err := driver.FindElement(selenium.ByName, "query-builder-test")
 	if err != nil {
-		log.Fatalf("Cannot search project: %v", err)
+		log.Fatalf("Search input did not appear: %v", err)
 	}
-	searchInput.SendKeys("MedicalDataExchange")
+	realInput.SendKeys("MedicalDataExchange" + selenium.EnterKey)
+
+	repoLink, err := driver.FindElement(
+		selenium.ByCSSSelector,
+		"a[class='Link__StyledLink-sc-1syctfj-0 prc-Link-Link-9ZwDx']",
+	)
+	if err != nil {
+		log.Fatalf("Repository link not found: %v", err)
+	}
+	repoLink.Click()
 
 	log.Println("✅ Search test PASSED successfully")
 	fmt.Println("Press ENTER to close the browser...")
